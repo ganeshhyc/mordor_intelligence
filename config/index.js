dbe943d2f48fdc3f35d49314a74461927c610422
ganeshhyc <id.ganesh123@gmail.com>
2019-06-19 11:34:06 +0530

    work init

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 var base_url = '';
 var app_url = base_url + 'application/view';
 var component_url = base_url + 'application/component';
+var api_endpoint = 'https://api.myjson.com/bins/co2lt';
 var myApp = angular.module("myApp",['ngRoute']);
